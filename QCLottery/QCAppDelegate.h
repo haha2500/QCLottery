3230c3fe854b6bf0c267af6009ceb8b78e64e8ab
haha2500 <haha2500@126.com>
2012-05-09 07:25:13 +0800

    Initial Commit

---
+++ QCLottery/QCAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  QCAppDelegate.h
+//  QCLottery
+//
+//  Created by 武 孙 on 12-5-9.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface QCAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ QCLottery/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  QCLottery
+//
+//  Created by 武 孙 on 12-5-9.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "QCAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([QCAppDelegate class]));
+    }
+}
